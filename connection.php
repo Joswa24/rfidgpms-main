713e1f06ef328659ec4d86b2c67e26c453ab70be
Joswa24 <joshuapastorpide10@gmail.com>
2025-09-01 11:20:49 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
   <?php
 date_default_timezone_set('Asia/Manila');
- $db = mysqli_connect('u8027114156_rfidgpmsPass','1rfidUser2025', '', 'u8027114156_rfidgpms') or
+ $db = mysqli_connect('u8027114156_rfidgpmsPass','1rfidUser2025', 'u8027114156_rfidgpms') or
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'u8027114156_rfidgpms' ) or die(mysqli_error($db));
 ?>
