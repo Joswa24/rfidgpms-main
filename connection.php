d17f01a4d95c1a284909a12a952d511a774dc7b7
bantayan_joiling <bantayan_joiling@gmail.com>
2025-09-02 14:38:37 +0800

    asd

--- connection.php
+++ connection.php
@@ -1,23 +1,10 @@
+   <?php
+date_default_timezone_set('Asia/Manila');
 
+ $db = mysqli_connect('127.0.0.1','1rfidUser2025','u8027114156_rfidgpmsPass','u8027114156_rfidgpms') or
 
-
-<?php
-
-
-$servername = "127.0.0.1";
-$username = "1rfidUser2025";
-$password = "u8027114156_rfidgpmsPass";
-$dbname = "u8027114156_rfidgpms";
-
-
-// Create connection
-$db = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-}
+        die ('Unable to connect. Check your connection parameters.');
+        mysqli_select_db($db, 'u8027114156_rfidgpms' ) or die(mysqli_error($db));
 ?>
-
 
 
