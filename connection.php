4f36b5fe6b4b86ed5173c9203fbea96f5ee5a4e5
Joswa24 <joshuapastorpide10@gmail.com>
2025-09-02 16:50:14 +0800

    commit

--- connection.php
+++ connection.php
@@ -7,4 +7,5 @@ date_default_timezone_set('Asia/Manila');
 
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'u8027114156_rfidgpms' ) or die(mysqli_error($db));
+        //sdfsdfew
 ?>
