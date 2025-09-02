acd5a6a20ed9da6e09863b750e2a76df5b78d175
bantayan_joiling <bantayan_joiling@gmail.com>
2025-09-02 14:29:12 +0800

    asdasd

--- connection.php
+++ connection.php
@@ -1,8 +1,28 @@
-   <?php
-date_default_timezone_set('Asia/Manila');
 
- $db = mysqli_connect('127.0.0.1','u8027114156_rfidgpmsPass','1rfidUser2025',  'u8027114156_rfidgpms') or
+<?php
+ $mysql_hostname = "localhost";
+$mysql_user = "u8027114156_rfidgpmsPass";
+$mysql_password = "1rfidUser2025";
+$mysql_database = "u8027114156_rfidgpmsPass";
+$bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database"); 
 
-        die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'u8027114156_rfidgpms' ) or die(mysqli_error($db));
-?>
+
+// $mysql_hostname = "localhost";
+// $mysql_user = "root";
+// $mysql_password = "";
+// $mysql_database = "foxrock_db";
+// $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
+
+ ?> 
+
+    //<?php
+//date_default_timezone_set('Asia/Manila');
+//
+ // $db = mysqli_connect('127.0.0.1','u8027114156_rfidgpmsPass','1rfidUser2025',  'u8027114156_rfidgpmsPass') or
+
+       // die ('Unable to connect. Check your connection parameters.');
+       // mysqli_select_db($db, 'u8027114156_rfidgpms' ) or die(mysqli_error($db));
+//?>
+
+
+
