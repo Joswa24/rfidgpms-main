9973be633837fa7e81c6b9525cf0716bbe6c1bee
Joswa24 <joshuapastorpide10@gmail.com>
2025-09-24 16:20:48 +0800

    wawaw

--- admin/index.php
+++ admin/index.php
@@ -152,6 +152,7 @@ function handleFailedLogin($maxAttempts, $lockoutTime) {
             display: flex;
             align-items: center;
             justify-content: center;
+            padding: 20px;
         }
         .login-container {
             background-color: white;
