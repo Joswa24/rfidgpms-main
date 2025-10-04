f7b5ce6f8b60cf5e5badb9075b0377dbcaa5f5b4
Joswa24 <joshuapastorpide10@gmail.com>
2025-10-05 07:37:37 +0800

    wawa

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 
   
-   <!-- <?php
+   <?php
 date_default_timezone_set('Asia/Manila');
 
  $db = mysqli_connect('127.0.0.1','u802714156_rfidgpmsPass','1rfidUser2025','u802714156_rfidgpms') or
 
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'u802714156_rfidgpms' ) or die(mysqli_error($db));
-?> -->
+?> 
 
   <?php
 // date_default_timezone_set('Asia/Manila');
