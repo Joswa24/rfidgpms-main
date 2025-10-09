314b92c1619725c6d4ad6a5a06ee3feda89fb056
Joswa24 <joshuapastorpide10@gmail.com>
2025-10-10 06:35:59 +0800

    wawa

--- admin/settings.php
+++ admin/settings.php
@@ -142,6 +142,8 @@ if ($result->num_rows > 0) {
     <script src="lib/tempusdominus/js/moment.min.js"></script>
     <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
     <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 
     <script>
         $(document).ready(function() {
