96abc1be39ed36e11a4a1d7d32bb611db4e271b4
Joswa24 <joshuapastorpide10@gmail.com>
2025-10-10 06:28:28 +0800

    wawaw

--- admin/instructors.php
+++ admin/instructors.php
@@ -387,6 +387,9 @@ include '../connection.php';
     <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
     <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+
     <script>
         $(document).ready(function() {
             // Initialize DataTable
