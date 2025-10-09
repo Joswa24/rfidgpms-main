2112ba27fd1cf9af2ab5b485e98063aa93f4649c
Joswa24 <joshuapastorpide10@gmail.com>
2025-10-10 06:34:34 +0800

    wawa

--- admin/manage_subjects.php
+++ admin/manage_subjects.php
@@ -338,6 +338,8 @@ include '../connection.php';
     <script src="lib/tempusdominus/js/moment.min.js"></script>
     <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
     <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 
     <script>
         $(document).ready(function() {
