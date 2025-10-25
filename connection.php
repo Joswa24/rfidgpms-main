339845c3527378bc95db9a975fe1d3018986c5e4
Joswa24 <joshuapastorpide10@gmail.com>
2025-10-25 13:59:13 +0800

    wawa

--- connection.php
+++ connection.php
@@ -14,10 +14,10 @@
 ?> 
 
 <?php
-date_default_timezone_set('Asia/Manila');
+// date_default_timezone_set('Asia/Manila');
 
- $db = mysqli_connect('localhost','root','','gpassdb') or
+//  $db = mysqli_connect('localhost','root','','gpassdb') or
 
-        die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'gpassdb' ) or die(mysqli_error($db));
+//         die ('Unable to connect. Check your connection parameters.');
+//         mysqli_select_db($db, 'gpassdb' ) or die(mysqli_error($db));
 ?>
