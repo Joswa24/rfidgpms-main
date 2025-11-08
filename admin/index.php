a74f3b4b6903e93b57f969cb50cc5c6931033aa4
Joswa24 <joshuapastorpide10@gmail.com>
2025-11-08 13:13:55 +0800

    wawa

--- admin/index.php
+++ admin/index.php
@@ -408,7 +408,7 @@ function send2FACodeEmail($email, $verificationCode) {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'joshuapastorpide10@gmail.com';
-        $mail->Password = 'ycplfxcclifaxxf';
+        $mail->Password = 'ydadaipirzhyhzae';
         $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
         $mail->Timeout = 30;
