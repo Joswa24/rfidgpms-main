77deb7c44fdf2fcee6e4a084405ee9aac967b629
Joswa24 <joshuapastorpide10@gmail.com>
2025-11-08 13:18:35 +0800

    wawa

--- admin/index.php
+++ admin/index.php
@@ -397,9 +397,9 @@ function send2FACodeEmail($email, $verificationCode) {
         }
 
         // Load PHPMailer with correct relative paths
-        require_once __DIR__ . '/../PHPMailer/src/PHPMailer.php';
-        require_once __DIR__ . '/../PHPMailer/src/SMTP.php';
-        require_once __DIR__ . '/../PHPMailer/src/Exception.php';
+        require_once __DIR__ . '/PHPMailer/src/PHPMailer.php';
+        require_once __DIR__ . '/PHPMailer/src/SMTP.php';
+        require_once __DIR__ . '/PHPMailer/src/Exception.php';
         
         $mail = new PHPMailer\PHPMailer\PHPMailer(true);
         
